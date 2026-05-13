de988b749ad8648c91db8be49628b2fe6496b814
cariay <cariay@cs.wallawalla.edu>
2026-05-13 13:25:08 -0700

    update

--- midi.py
+++ midi.py
@@ -36,31 +36,34 @@ def compose_scale_melody(scale_notes, length=32, bpm=120):
     tempo   = int(60_000_000 / bpm)
     quarter = TICKS
 
+def compose_fractal(base_notes, depth=3, bpm=100):
+    """Recursively repeat a motif at different scales."""
+    tempo   = int(60_000_000 / bpm)
+    quarter = TICKS
+
     mid   = MidiFile(ticks_per_beat=TICKS)
     track = MidiTrack()
     mid.tracks.append(track)
     track.append(mido.MetaMessage('set_tempo', tempo=tempo, time=0))
 
-    note = random.choice(scale_notes)
-    for _ in range(length):
-        duration = random.choice([quarter // 2, quarter, quarter * 2])
-        velocity = random.randint(60, 100)
-        add_note(track, note, duration, velocity)
-        # Step up, down, or jump in the scale
-        step = random.choice([-2, -1, -1, 0, 1, 1, 2])
-        idx  = scale_notes.index(note)
-        note = scale_notes[max(0, min(len(scale_notes)-1, idx + step))]
+    def motif(notes, dur, level):
+        if level == 0:
+            for n in notes:
+                add_note(track, n, dur)
+        else:
+            for n in notes:
+                motif([n, n+2, n+4], dur // 3, level - 1)
 
-    mid.save("melody.mid")
-    return "melody.mid"
+    motif(base_notes, quarter * 4, depth)
+    mid.save("fractal.mid")
+    return "fractal.mid"
 
-# A minor pentatonic starting at A3
-A_MINOR_PENTATONIC = [57, 60, 62, 64, 67, 69, 72, 74, 76]
-
-path = compose_scale_melody(A_MINOR_PENTATONIC, length=48, bpm=130)
+path = compose_fractal([60, 64, 67, 72], depth=3, bpm=80)
 
 pygame.mixer.init()
 pygame.mixer.music.load(path)
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy():
     pygame.time.Clock().tick(10)
+
+    
